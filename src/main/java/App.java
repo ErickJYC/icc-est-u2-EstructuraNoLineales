781495f29973742cbb6fd880040759472735db9f
Erick Yunga <eyungac3@est.ups.edu.ec>
2025-06-17 08:46:43 -0500

    feat(001) : Se a creado el ArbolBinario en inorden y preorden

---
+++ src/main/java/App.java
@@ -0,0 +1,20 @@
+import controllers.ArbolBinario;
+import models.Node;
+
+public class App {
+    public static void main(String[] args) {
+        ArbolBinario ab = new ArbolBinario();
+        ab.insert(50);
+        ab.insert(17);
+        ab.insert(76);
+        ab.insert(9);
+        ab.insert(23);
+        ab.insert(54);
+        ab.insert(14);
+        ab.insert(19);
+
+        ab.imprimirArbol();
+        ab.imprimirArbolPreorden();
+    }
+
+}
---
+++ src/main/java/controllers/ArbolBinario.java
@@ -0,0 +1,58 @@
+package controllers;
+
+import models.Node;
+
+public class ArbolBinario {
+    private Node root;
+    public ArbolBinario() {
+        this.root = null;
+    }
+
+    public void insert(int value){
+        root = insertRec(root, value);
+
+    }
+
+    private Node insertRec(Node padre, int value){
+        if (root == null){
+            return new Node(value);
+        }
+        if (value < padre.getValue())
+//            ME VOY A LA IZQUIERDA
+            padre.setLeft(insertRec(padre.getLeft(),value));
+
+         else if (value > padre.getValue()) {
+//            ME VOY A LA DERECHA
+            padre.setLeft(insertRec(padre.getRight(),value));
+
+        }
+        return padre;
+    }
+    public void imprimirArbol(){
+        imprimir(root);
+
+    }
+
+    private void imprimir(Node node){
+        if (node != null) {
+            imprimir(node.getLeft());
+            System.out.println(node.getValue() + " , ");
+            imprimir(node.getRight());
+        }
+    }
+//    Preorden
+    public void imprimirPreorden(Node node){
+        if (node != null){
+            System.out.println(node.getValue() + " , ");
+            imprimir(node.getLeft());
+            imprimir(node.getRight());
+        }
+
+    }
+    public void imprimirArbolPreorden(){
+        imprimirPreorden(root);
+    }
+//    public boolean buscar (int value){
+//        return buscar(root,value);
+//    }
+}
